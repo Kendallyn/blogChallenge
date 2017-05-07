debd633285d5f28877ec3fc3133ff56ba0abd30f
Kendallyn <Kendallynlyon@hotmail.com>
2017-05-06 17:09:43 -0700

    removed comments

--- server.js
+++ server.js
@@ -34,41 +34,24 @@ app.post('/blog-posts', jsonParser, (req, res) => {
     const requiredFields = ["id", "title", "content", "author", "publishDate"];
     for (let i = 0; i < requiredFields.length; i++) {
         const field = requiredFields[i];
-        if (!(fiend in req.body)) {
+        if (!(field in req.body)) {
             const message = `Missing \`${field}\` in request body`
             console.error(message);
             return res.status(400).send(message);
         }
     }
-    if (req.params.id !== req.body.id) {
-        const message = (
-            `Request path id (${req.params.id}) and request body id`
-            `(${req.body.id}) must match`);
-        console.error(message);
-        return res.status(400).send(message);
-    }
-    console.log(`Updating blog post \`${req.params.id}\``);
-    const updatedItem = BlogPosts.update({
-        id: req.params.id,
-        title: req.body.title,
-        content: req.body.content,
-        author: req.body.author,
-        pushlishDate: req.body.publishDate
-    });
-    res.status(204).json(updatedItem);
+
+    const item = BlogPosts.create(
+        req.body.title, req.body.content, req.body.author);
+    res.status(201).json(item);
 });
 
-app.delete('/blog-posts/:id', (req, res) => {
-    BlogPosts.delete(req.params.id);
-    console.log(`Deleted blog post \`${req.params.id}\``);
-    res.status(204).end();
-});
 
-app.put('/blog-posts/:id', (req, res) => {
+app.put('/blog-posts/:id', jsonParser(req, res) => {
     const requiredFields = ["id", "title", "content", "author", "publishDate"];
     for (let i = 0; i < requiredFields.length; i++) {
         const field = requiredFields[i];
-        if (!(fiend in req.body)) {
+        if (!(field in req.body)) {
             const message = `Missing \`${field}\` in request body`
             console.error(message);
             return res.status(400).send(message);
@@ -81,7 +64,7 @@ app.put('/blog-posts/:id', (req, res) => {
         console.error(message);
         return res.status(400).send(message);
     }
-    console.log(`Updating blog post \`${req.params.id}\``);
+    console.log(`Updating blog post with id \`${req.params.id}\``);
     const updatedItem = BlogPosts.update({
         id: req.params.id,
         title: req.body.title,
@@ -91,6 +74,14 @@ app.put('/blog-posts/:id', (req, res) => {
     });
     res.status(204).json(updatedItem);
 });
+
+
+app.delete('/blog-posts/:id', (req, res) => {
+    BlogPosts.delete(req.params.id);
+    console.log(`Deleted blog post with id \`${req.params.id}\``);
+    res.status(204).end();
+});
+
 
 app.listen(process.env.PORT || 8080, () => {
     console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
